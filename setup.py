39ae48d80d9c7c94975b7939bb2ad85ed28696b0
Victor <victor.pavlov.vadimovich@gmail.com>
2025-03-27 02:12:49 +0300

    ci update

--- setup.py
+++ setup.py
@@ -6,6 +6,28 @@ from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 import pybind11
 
+
+base_compile_args = ["-lpthread", "-shared", "-std=c++2a", "-O4", "-fPIC"]
+compile_args = {
+    'win32': [],
+    'linux': [],
+    'darwin': ['-I $(brew --prefix libomp)/include', '-L $(brew --prefix libomp)/lib'] 
+}
+
+base_link_args = ["-std=c++2a", "-lpthread"]
+link_args = {
+    'win32': [],
+    'linux': [],
+    'darwin': []
+}
+
+# Choose args based on the current platform
+current_platform = sys.platform
+extra_compile_args = base_compile_args + compile_args.get(current_platform, [])
+extra_link_args = base_link_args + link_args.get(current_platform, [])
+
+
+
 # Define the C++ extension
 ext_modules = [
     Extension(
@@ -13,8 +35,8 @@ ext_modules = [
         sources=["netlibpp/src/include/graph_func.cpp"],
         include_dirs=[pybind11.get_include()],
         language="c++",
-        extra_compile_args=["-lpthread", "-shared", "-std=c++2a", "-O4", "-fPIC"],
-        extra_link_args=["-std=c++2a", "-lpthread"]
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args
     ),
 ]
 
