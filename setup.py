215feb70e3176baeeef8cf8dc33fabc65af3bf87
Victor <victor.pavlov.vadimovich@gmail.com>
2025-04-02 04:52:39 +0300

    macos-12 change

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ extra_compile_args = base_compile_args + compile_args.get(current_platform, [])
 extra_link_args = base_link_args + link_args.get(current_platform, [])
 
 if current_platform == 'darwin':
-    os.environ["CC"] = "g++-13"
+    os.environ["CC"] = "gcc-13"
     os.environ["CXX"] = "g++-13"
 
     # omp_include = f"{get_brew_path('libomp')}/include"
